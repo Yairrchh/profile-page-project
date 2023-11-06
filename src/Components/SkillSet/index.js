b3d39b454773bc23bb1b8762efbaeaaa60a183cf
unknown <chirayair@gmail.com>
2023-11-05 21:22:02 -0400

    home improvements

--- src/Components/SkillSet/index.js
+++ src/Components/SkillSet/index.js
@@ -8,7 +8,7 @@ const SkillSet = () => {
     const context = useContext(profilePageContext)
 
     return (
-            <section>
+            <section className="mb-20">
                 <h3 className="flex justify-center items-center mt-6 text-4xl tracking-widest text-white mb-10">Skill set</h3>
                 <div className="flex flex-wrap mt-16 mx-7 justify-center items-center gap-16 grid-cols-div">
                     {
