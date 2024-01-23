167f3451121db081c996db00df508a5b4c1386d5
unknown <chirayair@gmail.com>
2024-01-22 21:20:33 -0400

    adding changes

--- src/dataBases/briefcaseBD.js
+++ src/dataBases/briefcaseBD.js
@@ -1,7 +1,7 @@
 const briefcaseBD = [
     {
         name: "Shopi Marketplace",
-        description: "Online store project using React.js, Vite.js and tailwind.css",
+        description: "Online store project using React.js, Vite.js and tailwind.css.",
         image: "../assets/briefcaseImg/shopiOnline.png",
         linkDeploy: "https://clinquant-chimera-0ea617.netlify.app/",
         linkRepository: "https://github.com/Yairrchh/react-proyect-vite-tailwind",
@@ -17,7 +17,7 @@ const briefcaseBD = [
     },
     {
         name: "Video game",
-        description: "video game project made with javaScript.js",
+        description: "video game project made with javaScript.js.",
         image: "../assets/briefcaseImg/videoGame.png",
         linkDeploy: "https://darling-trifle-8f5e5d.netlify.app/",
         linkRepository: "https://github.com/Yairrchh/taller-practico-js-videogames",
@@ -25,14 +25,14 @@ const briefcaseBD = [
     },
     {
         name: "Blog React-Routers-Dom",
-        description: "This is a project to implement React-router-Dom and private routes",
+        description: "This is a project to implement React-router-Dom and private routes.",
         image: "../assets/briefcaseImg/blogpost.png",
         linkDeploy: "https://gentle-treacle-4e1b38.netlify.app/",
         linkRepository: "https://github.com/Yairrchh/hyperblog1.0",
     },
     {
         name: "Work done at Olax store",
-        description: "Page made with vite, tailwind.css, react and applying react-router-dom",
+        description: "Page made with vite, tailwind.css, react and applying react-router-dom.",
         image: "../assets/briefcaseImg/work-olax.png",
         linkDeploy: "https://olaxlatamoficial.com/",
         linkRepository: "https://github.com/Yairrchh/work-olax-page"
@@ -46,14 +46,14 @@ const briefcaseBD = [
     },
     {
         name: "Concert Codplay",
-        description: "Page made with Xstate to create a state machine",
+        description: "Page made with Xstate to create a state machine.",
         image: "../assets/briefcaseImg/page-machine-state.png",
         linkDeploy: "https://cozy-axolotl-04f769.netlify.app/",
         linkRepository: "https://github.com/Yairrchh/project-react-state-machine",
     },
     {
         name: "My profile page",
-        description: "Page made with webpack, tailwind.css and react",
+        description: "Page made with webpack, tailwind.css and react.",
         image: "../assets/briefcaseImg/my-profile-page.png",
         linkDeploy: "https://classy-crepe-8f2013.netlify.app/",
         linkRepository: "https://github.com/Yairrchh/profile-page-project",
