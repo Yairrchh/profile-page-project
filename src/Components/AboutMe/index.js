a5bf08754cbf0f2bdbd8cdbfdacd8b08511b50b6
unknown <chirayair@gmail.com>
2024-01-25 00:07:16 -0400

    adding changes to text

--- src/Components/AboutMe/index.js
+++ src/Components/AboutMe/index.js
@@ -8,7 +8,8 @@ const AboutMe = () => {
                 <div className="div-h1-h3-p w-96 text-white flex flex-col items-center mx-4">
                     <h1 className="text-h1 text-7xl mt-5">Welcome.</h1>
                     <h3 className="text-xl font-medium mt-5">About me</h3>
-                    <p className="font-light mt-4">Passionate about web development and knowledgeable about HTML, CSS, JavaScript, React and NodeJS.
+                    <p className="font-light mt-4">Passionate about web development and knowledgeable about HTML, CSS, JavaScript, typeScript,
+                        Git, Git-hub, React, Tailwind.css, Redux, NodeJS, MySQL, PostgreSQL and Xstate.
                         Active Platzi student with several certifications in the Front-End area.
                         Able to create responsive, dynamic and attractive websites for different types of
                         clients and users. Eager to learn new technologies and tools.
